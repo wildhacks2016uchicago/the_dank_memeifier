41804c5d1734a7340ac7ed433f76e4664a3b22c9
juliettehainline <juliette.hainline@gmail.com>
2016-11-19 01:48:07 -0600

    hello world

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-// handler receiving messages
-app.post('/webhook', function(req, res) {
-	var events = req.body.entry[0].messaging;
-	for (i = 0; i < events.length; i++) {
-		var event = events[i];
-		if (event.message && event.message.text) {
-			sendMessage(event.sender.id, {
-				text: "Echo: " + event.message.text
-			});
-		}
-	}
-	res.sendStatus(200);
-});
+'use strict'
 
-// generic function sending messages
-function sendMessage(recipientId, message) {
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {
-			access_token: process.env.PAGE_ACCESS_TOKEN
-		},
-		method: 'POST',
-		json: {
-			recipient: {
-				id: recipientId
-			},
-			message: message,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending message: ', error);
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error);
-		}
-	});
-};
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function(req, res) {
+	res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'))
+})
