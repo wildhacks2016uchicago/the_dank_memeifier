fc22e0e769c6e51368725107eeb168d66072f2a5
juliettehainline <juliette.hainline@gmail.com>
2016-11-19 22:32:31 -0600

    promises

--- text_on_image.js
+++ text_on_image.js
@@ -1,62 +1,93 @@
 // Set up imageMagick
+var Promise = require("bluebird");
+
 var fs = require('fs'),
 	gm = require('gm').subClass({
 		imageMagick: true
 	});
 
-function resize_picture(original_picture) {
-	gm(original_picture)
-		.resize(200, 200)
-		.write('kittens2.jpg', function(err) {
-			if (!err) console.log('Resize Done');
-			if (err) console.log('Resize failed');
-		});
-}
-
-function add_white(resized_picture) {
-	var width_white = 50;
-	var height_white = 200;
-	gm(width_white, height_white, "white")
-		.append('kittens2.jpg').append(true)
-		.write('kittens3.jpg', function(err) {
-			if (!err) {
-				console.log('WORKED: Centered our image.\n');
-			}
-			if (err) {
-				console.log('ERROR: Did not center image.\n');
-			}
-		});
-}
-}
-
-function draw_caption(text) {
-	var width = 300;
-	var height = 100;
-	gm(width, height, "white")
-		.fontSize(18)
-		.font('Helvetica Neue')
-		.drawText(20, 20, text)
-		.write("writingsomething.jpg", function(err) {
-			if (!err) console.log('WORKED: Writing our caption.\n');
-			if (err) console.log('ERROR: Caption didnt write.\n');
-		});
-}
-
-function append_caption_image(actual_image, caption_image) {
-	gm(caption_image)
-		.append(actual_image)
-		.write('final.png', function(err) {
-			if (!err) console.log('WORKED: Appending Worked.');
-			if (err) console.log('ERROR: Appending not working sry.');
-		});
-}
-
 function main(original_picture, text) {
-	resize_picture(original_picture);
-	add_white('kittens2.jpg');
-	draw_caption(text);
-	append_caption_image('kittens3.jpg', 'writingsomething.jpg');
+	var resize_promise = new Promise(function(resolve, reject) {
+		gm(original_picture)
+			.resize(200, 200)
+			.write('kittens2.jpg', function(err) {
+				if (!err) {
+					resolve("Resize Stuff worked!");
+					console.log('Resize Done');
+				}
+				if (err) {
+					reject(Error("Resize didn't happen"));
+					console.log('Resize failed');
+				}
+			});
+	});
+	var add_white_promise = resize_promise.then(function(resolve, reject) {
+		return new Promise(function(resolve, reject) {
+			var width_white = 50;
+			var height_white = 200;
+			gm(width_white, height_white, "white")
+				.append('kittens2.jpg').append(true)
+				.write('kittens3.jpg', function(err) {
+					if (!err) {
+						resolve("Add White Stuff Worked!");
+						console.log('WORKED: Centered our image.\n');
+					}
+					if (err) {
+						reject(Error("Add white didn't happen"));
+						console.log('ERROR: Did not center image.\n');
+					}
+				});
+		})
+	});
+	var draw_caption_promise = add_white_promise.then(function(resolve, reject) {
+		return new Promise(function(resolve, reject) {
+			var width = 300;
+			var height = 100;
+			gm(width, height, "white")
+				.fontSize(18)
+				.font('Helvetica Neue')
+				.drawText(20, 20, text)
+				.write("writingsomething.jpg", function(err) {
+					if (!err) {
+						resolve("Draw caption worked!");
+						console.log('WORKED: Writing our caption.\n');
+					}
+					if (err) {
+						reject(Error("Add white didn't happen"));
+						console.log('ERROR: Caption didnt write.\n');
+					}
+				});
+		})
+	});
+	var append_caption_image = draw_caption_promise.then(function(resolve, reject) {
+		return new Promise(function(resolve, reject) {
+			gm('writingsomething.jpg')
+				.append('kittens3.jpg')
+				.write('final.png', function(err) {
+					if (!err) {
+						resolve("Append caption worked!");
+						console.log('WORKED: Appending Worked.');
+					}
+					if (err) {
+						reject(Error("append caption didnt work lmao"));
+						console.log('ERROR: Appending not working sry.');
+					}
+				});
+		})
+	});
+	return append_caption_image.then(function(resolve, reject) {
+		return resize_promise.value() + add_white_promise.value() + draw_caption_promise.value() + append_caption_image.value();
+	});
 }
 
 main('therock.jpg', "this is rly dumb");
+
+//
+// 	resize_picture(original_picture);
+// 	add_white('kittens2.jpg');
+// 	draw_caption(text);
+// 	append_caption_image('kittens3.jpg', 'writingsomething.jpg');
+// }
+//
 // main('therock.jpg', "this is rly dumb");
+// // main('therock.jpg', "this is rly dumb");
