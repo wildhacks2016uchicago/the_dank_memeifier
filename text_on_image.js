4712e67457d7ecdb289df404c79cec9ac71ac144
juliettehainline <juliette.hainline@gmail.com>
2016-11-19 20:36:17 -0600

    made a meme with pictures lol

--- text_on_image.js
+++ text_on_image.js
@@ -4,42 +4,55 @@ var fs = require('fs'),
 		imageMagick: true
 	});
 
-// Set width and height of the caption.
-var width = 300;
-var height = 100;
+function resize_picture(original_picture) {
+	gm(original_picture)
+		.resize(200, 200)
+		.write('kittens2.jpg', function(err) {
+			if (!err) console.log('Resize Done');
+			if (err) console.log('Resize failed');
+		});
+}
 
-// Reference image to call later.
-gm('kittens.png')
-	.resize(200, 200)
-	.write('kittens.jpg', function(err) {
-		if (!err) console.log('done');
-		if (err) console.log('resize failed');
-	});
+function add_white(resized_picture) {
+	var width_white = 50;
+	var height_white = 200;
 
-var width1 = 50;
-var height1 = 200;
+	gm(width_white, height_white, "white")
+		.append('kittens2.jpg').append(true)
+		.write('kittens3.jpg', function(err) {
+			if (!err) console.log('WORKED: Centered our image.\n');
+			if (err) console.log('ERROR: Did not center image.\n');
+		});
+}
 
-gm(width1, height1, "white")
-	.append('kittens.jpg').append(true)
-	.write('kittens.jpg', function(err) {
-		if (!err) console.log('WORKED: Centered our image.\n');
-		if (err) console.log('ERROR: Did not center image.\n');
-	});
+function draw_caption(text) {
+	var width = 300;
+	var height = 100;
+	gm(width, height, "white")
+		.fontSize(18)
+		.font('Helvetica Neue')
+		.drawText(20, 20, text)
+		.write("writingsomething.jpg", function(err) {
+			if (!err) console.log('WORKED: Writing our caption.\n');
+			if (err) console.log('ERROR: Caption didnt write.\n');
+		});
+}
 
-// Draw the caption
-gm(width, height, "white")
-	.fontSize(18)
-	.font('Helvetica Neue')
-	.drawText(20, 20, "obama: ily joe\nbiden: ily too\nobama: ma vp lmao\nbiden: ayy lmao")
-	.write("writingsomething.jpg", function(err) {
-		if (!err) console.log('WORKED: Writing our caption.\n');
-		if (err) console.log('ERROR: Caption didnt write.\n');
-	});
+function append_caption_image(actual_image, caption_image) {
+	gm(caption_image)
+		.append(actual_image)
+		.write('final.png', function(err) {
+			if (!err) console.log('WORKED: Appending Worked.');
+			if (err) console.log('ERROR: Appending not working sry.');
+		});
+}
 
-// Append the caption and the image
-gm('writingsomething.jpg')
-	.append('kittens.jpg')
-	.write('crazy.png', function(err) {
-		if (!err) console.log('WORKED: Appending Worked.');
-		if (err) console.log('ERROR: Appending not working sry.');
-	});
+function main(original_picture, text) {
+	resize_picture(original_picture);
+	add_white('kittens2.jpg');
+	draw_caption(text);
+	append_caption_image('kittens3.jpg', 'writingsomething.jpg');
+}
+
+main('therock.jpg', "this is rly dumb");
+// main('therock.jpg', "this is rly dumb");
